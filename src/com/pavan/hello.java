b5c640978253bdd047ee37a653e4166d3caff99d
Pavan Yalamanchili <ypavanchowdary@outlook.com>
2017-07-11 11:59:26 -0400

    hello world projet setup

---
+++ src/com/pavan/hello.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.pavan;
+
+/**
+ * @author ypava
+ *
+ */
+public class hello {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
